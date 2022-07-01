179f0d51a5c6b33b6236997188d4f355904e7be9
John Barry <johnjbarry@gmail.com>
2022-07-01 15:22:01 +0100

    Changes for module2

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+mongoose.Promise = global.Promise;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost/productsdb');
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
